3474f7d77013b28df3c0deb19f71e4f07e82a7bf
Umair Javed <umairjaved728@gmail.com>
2023-11-16 22:51:29 +0500

    Fixing controllers and debugging

--- EcommerceWeb/Controllers/CategoryController.cs
+++ EcommerceWeb/Controllers/CategoryController.cs
@@ -27,5 +27,98 @@ namespace EcommerceWeb.Controllers
             //didn't passed object no need as it is category type, it will create itself new category object with the default values.
         }
 
+		//Create a new resource that is got in form if it does not exist. 
+		[HttpPost]
+        //when post is hit, we will be getting the category object
+        //No need to write queries, entity framework is handling everything
+        public IActionResult Create(Category obj)
+        {
+			if(obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+			}
+
+            //if display and name both are valid then only it will continue to add in database
+            if (ModelState.IsValid)
+			{
+                //Getting which object to add in database
+                _db.Categories.Add(obj);
+
+                //category inserted in data base
+                _db.SaveChanges();
+
+                //Temp data will render when we move to index page after creating data
+                TempData["success"] = "Category Created Successfully";
+
+				//return back to index page
+				return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Get action
+        //which id to remove
+        //? shows initially id is missing
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id==0) 
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+ 
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        { 
+            if (ModelState.IsValid)
+            {
+                //It will update obj based on id
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Need to specify here as method is different
+
+        [HttpPost, ActionName("Delete")]
+        //we write different method here as the parameters of both functions were same 
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+        }
     }
 }
--- EcommerceWeb/Models/Category.cs
+++ EcommerceWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcommerceWeb.Models
 {
@@ -7,10 +8,21 @@ namespace EcommerceWeb.Models
 		//primary key of table
 		[Key]
 		public int Id { get; set; }
+
+
 		//name of the order
 		[Required]
+		//Validations
+		//max length of name can be 30 characters
+		[MaxLength(30)]
+		//It tells how they must be printed on screen 
+		[DisplayName("Category Name")]
 		public string Name { get; set; }
-		//We have a lot of categories which category to display first on the page
-		public int DisplayOrder { get; set; }
+
+
+        [DisplayName("Display Order")]
+		[Range(1,100)]
+        //We have a lot of categories which category to display first on the page
+        public int DisplayOrder { get; set; }
 	}
 }
